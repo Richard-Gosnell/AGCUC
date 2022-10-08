c46572bd734abefcbc1b7f1bb6a2a29cf7f7e8a0
Richard <richardshanegosnell@gmail.com>
2022-10-07 22:42:10 -0500

    Create index.php
    Start location for header. Move to header file later.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Atlantic-Garden City United Church</title>
+    </head>
+    <body>
+        <header>
+            <h1>Atlantic-Garden City United Church</h1>
+        </header>
+    </body>
+</html>
