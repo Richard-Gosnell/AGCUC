cdd4a7a0317e3f2109cde0d79c9ee01df3e11f91
Richard <richardshanegosnell@gmail.com>
2022-10-18 21:00:53 -0500

    Added buttons
    Social media buttons added.

--- Shared_Templates/header_template.php
+++ Shared_Templates/header_template.php
@@ -1,7 +1,15 @@
 <?php
 
 ?>
-
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Atlantic-Garden City United Church</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <div id="fb-root"></div>
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0" nonce="U9LcYkJa"></script>
+</head>
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">Atlantic-Garden City United Church</a>
--- index.php
+++ index.php
@@ -8,10 +8,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Atlantic-Garden City United Church</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <div id="fb-root"></div>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0" nonce="3P7CWLbT"></script>
     </head>
     <body>
         <header>
             <?php require "Shared_Templates/header_template.php"; ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+        </header>
+        <footer>
+            <?php require "Shared_Templates/footer_template.php"; ?>
+        </footer>
     </body>
 </html>
