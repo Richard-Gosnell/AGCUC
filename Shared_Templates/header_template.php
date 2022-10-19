2ed88876c4f49c31a8043165447be5957d5974c9
Richard <richardshanegosnell@gmail.com>
2022-10-18 21:06:28 -0500

    Styles added to buttons

--- Shared_Templates/header_template.php
+++ Shared_Templates/header_template.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Atlantic-Garden City United Church</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="Styles/Main.css">
     <div id="fb-root"></div>
     <div id="fb-root"></div>
     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0" nonce="U9LcYkJa"></script>
