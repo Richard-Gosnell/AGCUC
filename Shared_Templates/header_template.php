93e9ee5efa132f5e47c82b2e07b3f00648e444ec
Richard <richardshanegosnell@gmail.com>
2022-10-18 19:55:12 -0500

    Repaired templates

--- Shared_Templates/header_template.php
+++ Shared_Templates/header_template.php
@@ -1,11 +1,60 @@
 <?php
-echo '
-    <html>
-        <header>
-            <h1>Atlantic-Garden City United Church</h1>
-        </header>
-    </html>
-    '
+
 ?>
 
-
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Atlantic-Garden City United Church</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="about.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        About
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="mission.php">Our Mission</a></li>
+                        <li><a class="dropdown-item" href="staff.php">Our Staff</a></li>
+                        <li><a class="dropdown-item" href="history.php">Our History</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="worship.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Worship
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="rural_connect.php">Rural Connect</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="community.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Community
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="groups.php">Groups</a></li>
+                        <li><a class="dropdown-item" href="pastoral_care.php">Pastoral Care</a></li>
+                        <li><a class="dropdown-item" href="events.php">Events</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="donate.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Donate
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="memorial_fund.php">Memorial Fund</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
+</header>
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 ?>
 
 <!DOCTYPE html>
@@ -12,62 +11,7 @@
     </head>
     <body>
         <header>
-            <nav class="navbar navbar-expand-lg bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="index.php">Atlantic-Garden City United Church</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="about.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    About
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="mission.php">Our Mission</a></li>
-                                    <li><a class="dropdown-item" href="staff.php">Our Staff</a></li>
-                                    <li><a class="dropdown-item" href="history.php">Our History</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="worship.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Worship
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="rural_connect.php">Rural Connect</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="community.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Community
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="groups.php">Groups</a></li>
-                                    <li><a class="dropdown-item" href="pastoral_care.php">Pastoral Care</a></li>
-                                    <li><a class="dropdown-item" href="events.php">Events</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="donate.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Donate
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="memorial_fund.php">Memorial Fund</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
-        </header>
+            <?php require "Shared_Templates/header_template.php"; ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </body>
 </html>
