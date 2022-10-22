793ce5a2910c67c4f79bfcad9d99a161d8e35dca
Richard <richardshanegosnell@gmail.com>
2022-10-21 21:29:22 -0500

    Centered social media icon and added main css

--- Shared_Templates/footer_template.php
+++ Shared_Templates/footer_template.php
@@ -1,4 +1,12 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<a href="https://www.facebook.com/AGCUC" class="fa fa-facebook"></a>
-<a href="https://www.youtube.com/channel/UCaI3ZkX4uU916nk_5jnUh5w" class="fa fa-youtube"></a>
+<div class="container text-center">
+    <div class="row">
+        <div class="col">
+            <a href="https://www.facebook.com/AGCUC" class="fa fa-facebook"></a>
+            <a href="https://www.youtube.com/channel/UCaI3ZkX4uU916nk_5jnUh5w" class="fa fa-youtube"></a>
+        </div>
+    </div>
+</div>
+
+
