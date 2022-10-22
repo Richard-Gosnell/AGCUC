0b0067132655eb04a5b0dd706ede113ae4c63f26
Richard <richardshanegosnell@gmail.com>
2022-10-21 21:34:54 -0500

    Adding basic template links to pages

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
     <body>
         <header>
             <?php require "Shared_Templates/header_template.php"; ?>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         </header>
         <footer>
             <?php require "Shared_Templates/footer_template.php"; ?>
