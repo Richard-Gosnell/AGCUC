1054fb7b859a59464928ef000fa3d3ed1bdf97a6
Richard <richardshanegosnell@gmail.com>
2022-10-21 23:10:37 -0500

    Add image as link
    Not functional

--- Shared_Templates/footer_template.php
+++ Shared_Templates/footer_template.php
@@ -1,12 +1,11 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<div class="container text-center">
-    <div class="row">
-        <div class="col">
-            <a href="https://www.facebook.com/AGCUC" class="fa fa-facebook"></a>
-            <a href="https://www.youtube.com/channel/UCaI3ZkX4uU916nk_5jnUh5w" class="fa fa-youtube"></a>
-        </div>
-    </div>
+<div class="btn-group btn-group-lg" role="group" aria-label="Large button group">
+    <button type="button" class="btn btn-outline-dark">
+        <a href="https://www.facebook.com/AGCUC"><img src="../Images/Facebook_logo.png"></a>
+        <a href="https://www.youtube.com/channel/UCaI3ZkX4uU916nk_5jnUh5w" class="fa fa-youtube"></a>
+    </button>
 </div>
+
 
 
