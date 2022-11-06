9ed0c220b4f3f6e32f23459d27692e048b7cc32c
Richard <richardshanegosnell@gmail.com>
2022-11-06 16:32:19 -0600

    Update pastoral_care.php

--- pastoral_care.php
+++ pastoral_care.php
@@ -12,11 +12,36 @@
     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0" nonce="3P7CWLbT"></script>
 </head>
 <body>
-<header>
-    <?php require "Shared_Templates/header_template.php"; ?>
-</header>
-<footer>
-    <?php require "Shared_Templates/footer_template.php"; ?>
-</footer>
+    <header>
+        <?php require "Shared_Templates/header_template.php"; ?>
+    </header>
+    <section id="Pastoral_Care">
+        <h2>Atlantic-Garden City United Church Pastoral Care</h2>
+        <h5>The most wonderful places to be in the
+            world are in someone's thoughts,
+            someone's prayers and in someone's heart.
+            - Author Unknown</h5>
+        <p>
+            <i>Listening, Supporting, Encouraging, Befriending</i>
+        </p>
+        <p>All four words are linked to the idea of caring: <br>
+            In listening, if we do not hear, we cannot help.  When one listens he or she lets the person or group know
+            they are valued.  By supporting, we try to offer encouragement by our words and our actions.  Befriending,
+            we hope to be a friend especially when someone is in need of help or support; through visits and calls.
+            Encouraging others can make such a big difference in a person's life.  We try to encourage our church family
+            and others to stay strong, stay positive and to keep the faith.</p>
+        <ul>
+            <p>Our minister provides leadership and support in helping pastoral care "reach out".<br>
+                The pastoral care team at Atlantic Garden City United Church strives to:</p>
+            <li>To be welcoming and hospitable to newcomers.</li>
+            <li>To maintain regular contact with shut-ins at home or in long term care.</li>
+            <li>To express care and concern for our church families’ day to day lives.</li>
+            <li>To acknowledge that pastoral care is the concern of the entire congregation.</li>
+            <li>To celebrate gifts that people bring to the life of our church</li>
+        </ul>
+    </section>
+    <footer>
+        <?php require "Shared_Templates/footer_template.php"; ?>
+    </footer>
 </body>
 </html>
