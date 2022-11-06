680ab4b7038b5c5649bfb5939e6ddce4ac44f838
Richard <richardshanegosnell@gmail.com>
2022-11-06 16:47:09 -0600

    Update memorial_fund.php

--- memorial_fund.php
+++ memorial_fund.php
@@ -12,11 +12,24 @@
     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0" nonce="3P7CWLbT"></script>
 </head>
 <body>
-<header>
-    <?php require "Shared_Templates/header_template.php"; ?>
-</header>
-<footer>
-    <?php require "Shared_Templates/footer_template.php"; ?>
-</footer>
+    <header>
+        <?php require "Shared_Templates/header_template.php"; ?>
+    </header>
+    <section id="Memorial_Fund-Text">
+        <h2>Memorial Fund</h2>
+        <p><i>Gifts to the Memorial Fund help Atlantic-Garden City United Church carry out the mission of the church as
+                we work together to put our faith into action.</i></p>
+        <h5>MEMORIAL GIFTS: </h5>
+        <p>Given at the time of death to honour a loved one or on the anniversary of a loved one’s death.</p>
+        <h5>HONOUR GIFTS:</h5>
+        <p>Gifts given to acknowledge a special birthday, anniversary or just to say thank you.  Don’t wait until
+            someone has passed to give a special thank you.</p>
+        <a href="#">Please fill out this form and send it with your donation.</a>
+        <p><i>Gifts will be recorded in “The Book of Remembrance” which is placed on display in the church.</i></p>
+        <p><i>The Memorial Fund Committee welcomes your questions and your suggestions.</i></p>
+    </section>
+    <footer>
+        <?php require "Shared_Templates/footer_template.php"; ?>
+    </footer>
 </body>
 </html>
